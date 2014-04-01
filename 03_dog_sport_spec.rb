4ed03a50d55d11b14439394770d40b49b0fbd892
Kinsey Durham <kinseyann505@gmail.com>
2014-04-01 16:15:02 -0600

    Spec 4 for hash warmup

---
+++ 03_dog_sport_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+describe 'Dog Hash' do
+  it "returns Harleigh's favorite sports" do
+    expect(DOG["harleigh"][:information][:sport]).to eq(["frisbee", "hiking"])
+  end
+
+  it "returns Trixie's favorite sports" do
+    expect(DOG["trixie"][:information][:sport]).to eq([:swimming, :prancing])
+  end
+end
