e38e6dddc631b2356f3872cf5a66c74fb8898603
Kinsey Durham <kinseyann505@gmail.com>
2014-04-02 10:02:39 -0600

    Renamed and fixed test description for spec 2

--- 01_dog_keys_spec.rb
+++ 01_dog_keys_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'rspec/autorun'
 
 describe 'Dog Hash' do
-  it 'returns the color of the dog' do
+  it 'returns the values of the first_name and color keys' do
     dog_attributes = (DOG[:cattle_dog][:preferences])
     expect(dog_attributes.values).to eq(['Harleigh', 'brown'])
   end
