e9b8d78aa83dcf17f53bfa9ca855be8b1243fb08
Kinsey Durham <kinseyann505@gmail.com>
2014-04-02 10:30:17 -0600

    Change preferences to names and remove variable

--- 01_dog_keys_spec.rb
+++ 01_dog_keys_spec.rb
@@ -3,7 +3,6 @@ require 'rspec/autorun'
 
 describe 'Dog Hash' do
   it 'returns the values of the first_name and color keys' do
-    dog_attributes = (DOG[:cattle_dog][:preferences])
-    expect(dog_attributes.values).to eq(['Harleigh', 'brown'])
+    expect(DOG[:cattle_dog][:names]).to eq(['Harleigh', 'brown'])
   end
 end
